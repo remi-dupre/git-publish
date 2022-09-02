2edbace2588519aa18f681c265a48a4f2f28484e
Rémi Dupré <remi@dupre.io>
2022-09-02 16:17:02 +0200

    delegate credential management to git2_credential

--- src/git_utils/mod.rs
+++ src/git_utils/mod.rs
@@ -1,38 +1,31 @@
 pub mod rebuilder;
 
 use anyhow::{Context, Result};
-use git2::{Commit, Cred, PushOptions, RemoteCallbacks, Repository};
+use git2::{Commit, PushOptions, RemoteCallbacks, Repository};
+use git2_credentials::CredentialHandler;
 
 const WORKING_BRANCH_NAME: &str = "git-publish_working_branch";
 
 pub fn build_remote_callbacks() -> Result<RemoteCallbacks<'static>> {
+    let git_config = git2::Config::open_default().unwrap();
     let mut cb = RemoteCallbacks::new();
+    let mut ch = CredentialHandler::new(git_config);
 
-    cb.credentials(|_url, username_from_url, _allowed_types| {
-        Cred::ssh_key(
-            username_from_url.unwrap(),
-            None,
-            std::path::Path::new(&format!(
-                "{}/.ssh/id_ed25519",
-                std::env::var("HOME").unwrap()
-            )),
-            None,
-        )
-    })
-    .transfer_progress(|p| {
-        eprintln!("{}/{}", p.indexed_objects(), p.total_objects());
-        eprintln!("{}/{}", p.indexed_deltas(), p.total_deltas());
-        true
-    })
-    .push_update_reference(|reference, status| {
-        if let Some(msg) = status {
-            println!(r"/!\ failed to push {reference}: {msg}");
-        } else {
-            println!("Successfully pushed {reference}");
-        }
+    cb.credentials(move |url, username, allowed| ch.try_next_credential(url, username, allowed))
+        .transfer_progress(|p| {
+            eprintln!("{}/{}", p.indexed_objects(), p.total_objects());
+            eprintln!("{}/{}", p.indexed_deltas(), p.total_deltas());
+            true
+        })
+        .push_update_reference(|reference, status| {
+            if let Some(msg) = status {
+                println!(r"/!\ failed to push {reference}: {msg}");
+            } else {
+                println!("Successfully pushed {reference}");
+            }
 
-        Ok(())
-    });
+            Ok(())
+        });
 
     Ok(cb)
 }
@@ -53,7 +46,7 @@ pub fn push_reference(
     };
 
     let refspec = format!("{refspec_prefix}{src_reference}:{dst_reference}");
-    let mut remote = rep.remote_anonymous(&remote_url)?;
+    let mut remote = rep.remote_anonymous(remote_url)?;
     let mut push_opt = PushOptions::new();
     push_opt.remote_callbacks(build_remote_callbacks()?);
     remote.push(&[refspec], Some(&mut push_opt))?;
@@ -68,7 +61,7 @@ pub fn push_commit(
     force: bool,
 ) -> Result<()> {
     let mut src_ref = rep
-        .branch(WORKING_BRANCH_NAME, &commit, true)
+        .branch(WORKING_BRANCH_NAME, commit, true)
         .context("failed to create working branch")?
         .into_reference();
 
@@ -76,7 +69,7 @@ pub fn push_commit(
         .name()
         .context("failed to get source reference name")?;
 
-    let push_res = push_reference(rep, &src_ref_name, dst_reference, remote_url, force);
+    let push_res = push_reference(rep, src_ref_name, dst_reference, remote_url, force);
     src_ref.delete()?;
     push_res
 }
